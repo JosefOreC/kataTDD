7d21b707c8a290a4327f5f4cf595f3bbed5e9f7a
OJosef <71660137@continental.edu.pe>
2025-04-23 11:50:14 -0500

    Programa completado

--- src/logica/conjunto.py
+++ src/logica/conjunto.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Conjunto:
     def __init__(self, conjunto):
         self.__conjunto = conjunto
@@ -5,3 +7,9 @@ class Conjunto:
     def promedio(self):
         if not self.__conjunto: return None
         return sum(self.__conjunto)/len(self.__conjunto)
+
+num_elements = randint(1,50)
+elements= [randint(1,1000)for i in range(num_elements)]
+
+print(elements)
+print(Conjunto(elements).promedio())
--- test/TestConjunto.py
+++ test/TestConjunto.py
@@ -1,4 +1,5 @@
 import unittest
+from random import randint
 from src.logica.conjunto import Conjunto
 
 class TestConjunto(unittest.TestCase):
@@ -9,4 +10,11 @@ class TestConjunto(unittest.TestCase):
     def test_conjunto_promedio_returnPromedio(self):
         conjunto = Conjunto([1,3,4,5])
         promedio_esperado = 13/4
+        self.assertEqual(promedio_esperado, conjunto.promedio())
+
+    def test_conjunton_promedio_returnPromedio(self):
+        num_elements = randint(1,50)
+        elements= [randint(1,1000)for i in range(num_elements)]
+        promedio_esperado = sum(elements)/num_elements
+        conjunto = Conjunto(elements)
         self.assertEqual(promedio_esperado, conjunto.promedio())
