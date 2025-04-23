a8480bea07d95d090fe0ef6a3e5f6df833742fea
OJosef <71660137@continental.edu.pe>
2025-04-23 11:25:14 -0500

    Modulo promedio() definido y pasado pruebas.

--- src/logica/conjunto.py
+++ src/logica/conjunto.py
@@ -2,5 +2,6 @@ class Conjunto:
     def __init__(self, conjunto):
         self.__conjunto = conjunto
 
-    def promedio(self, promedio):
-        return False
+    def promedio(self):
+        if not self.__conjunto: return None
+        return sum(self.__conjunto)/len(self.__conjunto)
--- test/TestConjunto.py
+++ test/TestConjunto.py
@@ -2,5 +2,11 @@ import unittest
 from src.logica.conjunto import Conjunto
 
 class TestConjunto(unittest.TestCase):
-    pass
+    def test_conjunto_vacio_returnNone(self):
+        conjunto = Conjunto([])
+        self.assertIsNone(conjunto.promedio())
 
+    def test_conjunto_promedio_returnPromedio(self):
+        conjunto = Conjunto([1,3,4,5])
+        promedio_esperado = 13/4
+        self.assertEqual(promedio_esperado, conjunto.promedio())
